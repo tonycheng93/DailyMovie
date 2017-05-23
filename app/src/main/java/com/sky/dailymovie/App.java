a45a3f3230a77f5b822f64bab1402a489d339bf9
tonycheng <bc1341286335@gmail.com>
2017-05-23 22:20:04 +0800

    modify project build.gradle

--- app/src/main/java/com/sky/dailymovie/App.java
+++ app/src/main/java/com/sky/dailymovie/App.java
@@ -32,6 +32,6 @@ public class App extends Application {
         Fabric.with(this, new Crashlytics());
 
         long endTime = System.currentTimeMillis();
-        Timber.d("init Application cost: ",(endTime - startTime) + "ms.");
+        Timber.d("init Application cost: ", (endTime - startTime) + " ms.");
     }
 }
