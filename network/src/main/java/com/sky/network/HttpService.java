db3117440fa0d6c4fde9f1613e12cede454b64eb
tonycheng <bc1341286335@gmail.com>
2017-06-09 22:34:49 +0800

    add network library

---
+++ network/src/main/java/com/sky/network/HttpService.java
@@ -0,0 +1,15 @@
+package com.sky.network;
+
+/**
+ * 项目名称：DailyMovie
+ * 类描述：
+ * 创建人：tonycheng
+ * 创建时间：2017/6/9 22:29
+ * 邮箱：tonycheng93@outlook.com
+ * 修改人：
+ * 修改时间：
+ * 修改备注：
+ */
+
+public interface HttpService {
+}
