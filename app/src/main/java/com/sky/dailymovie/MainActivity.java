db1f57997cb544a146445faf8c76e7942c60044c
baocheng <baocheng@skyworth.com>
2017-06-16 18:28:49 +0800

    add something

--- app/src/main/java/com/sky/dailymovie/MainActivity.java
+++ app/src/main/java/com/sky/dailymovie/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity implements MainFragment.OnFr
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_copy);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
